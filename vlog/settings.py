9bb0fdedae5c3c36b073fbae3ce9004df4376b19
aakimovkgz <arturakimov1402@icloud.com>
2022-07-13 23:30:13 +0600

    Pre-deploy commit

--- vlog/settings.py
+++ vlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ztv3w5$ha_x)vgq30d_=7iq#yjlh^b(8@jn*nga_p&%szz2gi6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
